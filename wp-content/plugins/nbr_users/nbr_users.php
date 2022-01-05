e0ea45d176615fb0029602f9e786ca1b5b986ae0
Moulaïli Saïdi <91431031+Moulayli@users.noreply.github.com>
2022-01-05 11:43:56 +0100

    Update nbr_users.php

--- wp-content/plugins/nbr_users/nbr_users.php
+++ wp-content/plugins/nbr_users/nbr_users.php
@@ -21,6 +21,27 @@ function nbrusers() {
 parent::WP_Widget('ATL', $name = 'nonbre de visiteurs', array('description' => 'Affiche les nonbres de visiteurs du site ')); 
 }
 
+function visiteurs(){
+    if(file_exists('compteur_visites.txt'))
+    {
+            $compteur_f = fopen('compteur_visites.txt', 'r+');
+            $compte = fgets($compteur_f);
+    }
+    else
+    {
+            $compteur_f = fopen('compteur_visites.txt', 'a+');
+            $compte = 0;
+    }
+    if(!isset($_SESSION['compteur_de_visite']))
+    {
+            $_SESSION['compteur_de_visite'] = 'visite';
+            $compte++;
+            fseek($compteur_f, 0);
+            fputs($compteur_f, $compte);
+    }
+    fclose($compteur_f);
+    return $compte;
+    }
 //  Mise en forme 
 function widget($args,$instance) { 
 	
@@ -40,13 +61,11 @@ function widget($args,$instance) {
 	echo $before_title.$title.$after_title; 
 
 // Boucle pour afficher les articles 
-	echo '<ul>'; 
+	echo '<p>'; 
 
-	foreach ( $lastposts as $post ) : setup_postdata($post); ?> 
-		<li><a href="<?php echo get_permalink($post->ID); ?>"><?php echo $post->post_title; ?></a></li> 
-	<?php endforeach; 
+	echo visiteurs();
 
-	echo '</ul>'; 
+	echo '</p>'; 
 
 
 // HTML APRES WIDGET 
@@ -57,11 +76,7 @@ function widget($args,$instance) {
 function update($new_instance, $old_instance) { 
 
 	
-		$instance = $old_instance; 
-
-//Récupération des paramètres envoyés 
-		$instance['title'] = strip_tags($new_instance['title']); 
-		$instance['nb_posts'] = $new_instance['nb_posts']; return $instance; 
+	
 
 
 } 
@@ -71,26 +86,9 @@ function update($new_instance, $old_instance) {
 function form($instance) { 
 
 // Etape 1 - Définition des variables titre et nombre de post 
-$title = esc_attr($instance['title']); 
-$nb_posts = esc_attr($instance['nb_posts']); 
 
-// Etape 2 - Ajout des deux champs
-?> 
 
-	<p> 
-		<label for="<?php echo $this->get_field_id('title'); ?>"> 
-			<?php echo 'Titre:'; ?> 
-			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /> 
-		</label> 
-	</p> 
-
-	<p> 
-		<label for="<?php echo $this->get_field_id('nb_posts'); ?>"> 
-			<?php echo 'Nombre d\'articles:'; ?> <input class="widefat" id="<?php echo $this->get_field_id('nb_posts'); ?>" name="<?php echo $this->get_field_name('nb_posts'); ?>" type="text" value="<?php echo $nb_posts; ?>" /> 
-		</label> 
-	</p> 
-
-<?php 
+// Etape 2 - Ajout des deux champs 
 
 }
 
