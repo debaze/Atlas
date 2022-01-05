fc9d4f91a53d47799afe783d2d318ed5e36ff8c7
Moulaïli Saïdi <91431031+Moulayli@users.noreply.github.com>
2022-01-05 10:45:14 +0100

    widget

---
+++ wp-content/plugins/nbr_users/nbr_users.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: nonbre de visiteurs 
+Plugin URI: https://www.numelion.com/
+Description: Un simple plugin pour afficher les nonbres de visiteurs du site 
+Author: Numelion
+Version: 1.0.0
+*/
+
+?>
