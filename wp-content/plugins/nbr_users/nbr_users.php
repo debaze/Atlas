6dfb2b288d2502188454d5b9857b64444d791a61
Moulaïli Saïdi <91431031+Moulayli@users.noreply.github.com>
2022-01-06 10:02:15 +0100

    update widget

--- wp-content/plugins/nbr_users/nbr_users.php
+++ wp-content/plugins/nbr_users/nbr_users.php
@@ -19,7 +19,7 @@ class widgetArticleRecents extends WP_Widget {
 
 // Constructeur du widgets 
 function __construct() { 
-parent::__construct('widgetArticleRecents', 'Nombre de visiteur du site', array('description' => 'Affichage des Nombre de visiteur du site')); 
+	parent::__construct('widgetArticleRecents', 'Nombre de visiteur du site', array('description' => 'Affichage des Nombre de visiteur du site')); 
 }
 
 //  Mise en forme 
@@ -27,20 +27,18 @@ function widget($args,$instance) {
 	
 	extract($args); 
 
-	$title = apply_filters('widget_title', $instance['title']); 
-	$nb_posts = $instance['nb_posts']; 
+	//$title = apply_filters('widget_title', $instance['title']); 
+	//$nb_posts = $instance['nb_posts']; 
 	
 	//Récupération des articles 
 	
-	$lastposts = get_posts(array('numberposts'=>$nb_posts)); 
+	//$lastposts = get_posts(array('numberposts'=>$nb_posts)); 
 
-// HTML AVANT WIDGET 
 	echo $before_widget;
  
-// Titre du widget qui va s’afficher 
+
 	echo $before_title.$title.$after_title; 
 
-// Boucle pour afficher les articles 
 	echo '<p>'; 
 
         if(file_exists('compteur_visites.txt'))
@@ -67,54 +65,9 @@ function widget($args,$instance) {
 	echo '</p>'; 
 
 
-// HTML APRES WIDGET 
 	echo $after_widget; 
 }
 
-/*
-// Récupération des paramètres 
-function update($new_instance, $old_instance) { 
-
-	
-		$instance = $old_instance; 
-
-//Récupération des paramètres envoyés 
-		$instance['title'] = strip_tags($new_instance['title']); 
-		$instance['nb_posts'] = $new_instance['nb_posts']; return $instance; 
-
-
-} 
-
-
-// Paramètres dans l’administration 
-function form($instance) { 
-
-// Etape 1 - Définition des variables titre et nombre de post 
-$title = esc_attr($instance['title']); 
-$nb_posts = esc_attr($instance['nb_posts']); 
-
-// Etape 2 - Ajout des deux champs
-?> 
-
-	<p> 
-		<label for="<?php echo $this->get_field_id('title'); ?>"> 
-			<?php echo 'Titre:'; ?> 
-			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /> 
-		</label> 
-	</p> 
-
-	<p> 
-		<label for="<?php echo $this->get_field_id('nb_posts'); ?>"> 
-			<?php echo 'Nombre d\'articles:'; ?> <input class="widefat" id="<?php echo $this->get_field_id('nb_posts'); ?>" name="<?php echo $this->get_field_name('nb_posts'); ?>" type="text" value="<?php echo $nb_posts; ?>" /> 
-		</label> 
-	</p> 
-
-<?php 
-
-}
-*/
-
-// Fin du widget 
 }
 
 ?>
